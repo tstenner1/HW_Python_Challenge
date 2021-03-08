6c5094051b180c7eed929fa072a4735020d5d14c
tstenner1 <78450989+tstenner1@users.noreply.github.com>
2021-03-07 18:00:11 -0600

    Update PyPoll.py

--- Resources PyPoll/PyPoll.py
+++ Resources PyPoll/PyPoll.py
@@ -5,3 +5,8 @@ from collections import Counter
 
 voters_candidates = []
 votes_per_candidate = []
+
+election_data_csv_path = os.path.join("Resources","election_data.csv")
+
+#next step needs to be like pybank, open csv to read
+with open(election_data_csv_path, newline="") as csvfile: 
